8c906a220afd648b79a7a6620ef8bdbee6595eb9
hisamcode <hisamcode@gmail.com>
2023-09-05 20:21:26 +0700

    add table session in init.sql

--- db/init.sql
+++ db/init.sql
@@ -34,6 +34,14 @@ INSERT INTO snippets (title, content, created, expires) VALUES (
     DATE_ADD(UTC_TIMESTAMP(), INTERVAL 7 DAY)
 );
 
+CREATE TABLE sessions (
+    token CHAR(43) PRIMARY KEY,
+    data BLOB NOT NULL,
+    expiry TIMESTAMP(6) NOT NULL
+);
+
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
 CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
